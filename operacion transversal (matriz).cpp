a80707f0a362cc187074d066153529e85a5b53e6
Josué Álvarez <omarjosuealvarezreinaldo@gmail.com>
2020-05-01 02:02:20 +0200

    creo dos funciones para organizar los algoritmos de matriz

--- operacion transversal (matriz).cpp
+++ operacion transversal (matriz).cpp
@@ -1,11 +1,33 @@
-#include<stdio.h>
-
-int main(){
-    int MatrizLineal = {1,2,5,7,6};
+#include"stdio.h"
+using namespace std;
+int OperacionTranscersal(){
+    int MatrizLineal[] = {1,2,5,7,6};
     int Objeto = 10, k = 3, n = 5;
     int i = 0, j = n;
     printf("los elementos originales de la matriz son :\n");
     for (int i = 0; i < n; i++){
         printf("MatrizLineal[%d] = %d \n", i, MatrizLineal[i]);
     }
+}
+int OperacionIncercion(){
+    int MatrizLineal[] = {2,3,45,4,3};
+    int Objeto = 10, k = 3, n = 5; 
+    int i = 0, j = n;
+    printf("los elementos originales de la matriz son :\n");
+    for (int i = 0; i < n; i++){
+        printf("MatrizLineal[%d] = %d \n", i, MatrizLineal[i]);
+    }
+    n = n +1;
+    while( j >= k ){
+        MatrizLineal[j+1] = MatrizLineal[j];
+        j = j - 1 ;
+    }
+    MatrizLineal[k] = Objeto;
+    printf("los elementos del array despues de la insercion :\n");
+    for (int i = 0; i<n; i++){
+        printf("MatrizLineal[%d] = %d \n", i, MatrizLineal[i]);
+    }
+}
+int main(){
+    
 }
